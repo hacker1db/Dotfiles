18e9acf07fe3cd9ae05ca24e4a3f160399964e3d
hacker1db <hacker1db@outlook.com>
2017-07-20 23:23:15 -0700

    added gcm to aliass for git.zsh

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -12,6 +12,8 @@ alias gmv='git mv'
 alias grm='git rm'
 alias grn='git-rename'
 alias glog="git l"
+alias gcm="git commit -am"
+
 # alias git-amend='git commit --amend -C HEAD'
 alias git-undo='git reset --soft HEAD~1'
 alias git-count='git shortlog -sn'
