86d4097e3ffe9bb59e8345e8178bbc34895f2386
hacker1db <hacker1db@users.noreply.github.com>
2017-09-06 14:41:34 -0700

    Update brew.sh
    updated install for homebrew

--- install/brew.sh
+++ install/brew.sh
@@ -2,7 +2,7 @@
 
 if test ! $(which brew); then
     echo "Installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "Installing homebrew packages..."
