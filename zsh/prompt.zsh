8bd19dd66de3dc2d651b4ac5a1aab751f3b2ac45
hacker1db <hacker1db@outlook.com>
2023-10-01 17:28:16 -0700

    test out new promopt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,15 +1,47 @@
-# heavily inspired by the wonderful pure theme
-# https://github.com/sindresorhus/pure
-
-# needed to get things like current git branch
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable git # You can add hg too if needed: `git hg`
-zstyle ':vcs_info:git*' use-simple true
-zstyle ':vcs_info:git*' max-exports 2
-zstyle ':vcs_info:git*' formats ' %b' 'x%R'
-zstyle ':vcs_info:git*' actionformats ' %b|%a' 'x%R'
+autoload -Uz add-zsh-hook
+setopt prompt_subst
 
-autoload colors && colors
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git*' formats ' %b'
+
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+bold() {
+    echo -n "%B$1%b"
+}
+
+write() {
+    local color content bold
+    [[ -n "$1" ]] && color="%F{$1}" || color="%f"
+    [[ -n "$2" ]] && content="$2" || content=""
+
+    [[ -z "$2" ]] && content="$1"
+
+    echo -n "$color"
+    echo -n "$content"
+    echo -n "%{%b%f%}"
+}
+
+is_git() {
+    [[ $(command git rev-parse --is-inside-work-tree 2>/dev/null) == true ]]
+}
+
+PROMPT_SYMBOL='▷'
+
+# indicate a job (for example, vim) has been backgrounded
+# If there is a job in the background, display a ✱
+suspended_jobs() {
+    local sj
+    sj=$(jobs 2>/dev/null | tail -n 1)
+    if [[ $sj == "" ]]; then
+        echo ""
+    else
+        echo "%{%F{208}%}✱%f"
+    fi
+}
 
 node_prompt() {
     [[ -f package.json || -d node_modules ]] || return
@@ -26,61 +58,123 @@ node_prompt() {
     write '029' "$node_icon $version"
 }
 
-git_dirty() {
-    # check if we're in a git repo
-    command git rev-parse --is-inside-work-tree &>/dev/null || return
-
-    # check if it's dirty
-    command git diff --quiet --ignore-submodules HEAD &>/dev/null;
-    if [[ $? -eq 1 ]]; then
-        echo "%F{red}✗%f"
-    else
-        echo "%F{green}✔%f"
-    fi
+git_status_done() {
+    # $3 is the stdout of the git_status command
+    RPROMPT="$3 $(suspended_jobs)"
+    zle reset-prompt
 }
 
-# get the status of the current branch and it's remote
-# If there are changes upstream, display a ⇣
-# If there are changes that have been committed but not yet pushed, display a ⇡
-git_arrows() {
-    # do nothing if there is no upstream configured
-    command git rev-parse --abbrev-ref @'{u}' &>/dev/null || return
+git_status() {
+    cd -q "$1"
+    is_git || return
 
-    local arrows=""
-    local status
-    arrow_status="$(command git rev-list --left-right --count HEAD...@'{u}' 2>/dev/null)"
-
-    # do nothing if the command failed
-    (( !$? )) || return
-
-    # split on tabs
-    arrow_status=(${(ps:\t:)arrow_status})
-    local left=${arrow_status[1]} right=${arrow_status[2]}
-
-    (( ${right:-0} > 0 )) && arrows+="%F{011}⇣%f"
-    (( ${left:-0} > 0 )) && arrows+="%F{012}⇡%f"
-
-    echo $arrows
-}
-
-
-# indicate a job (for example, vim) has been backgrounded
-# If there is a job in the background, display a ✱
-suspended_jobs() {
-    local sj
-    sj=$(jobs 2>/dev/null | tail -n 1)
-    if [[ $sj == "" ]]; then
-        echo ""
-    else
-        echo "%{$FG[208]%}✱%f"
-    fi
-}
-
-precmd() {
     vcs_info
-    print -P '\n%F{51}%~'
+
+    local git_branch="$vcs_info_msg_0_"
+    git_branch="${git_branch#heads/}"
+    git_branch="${git_branch/.../}"
+
+    [[ -z "$git_branch" ]] && return
+
+    local INDEX git_status=""
+
+    GIT_SYMBOL=" "
+    GIT_STATUS_ADDED=$(write '002' ' ')
+    GIT_STATUS_MODIFIED=$(write '003' ' ')
+    GIT_STATUS_UNTRACKED=$(write '009' ' ')
+    GIT_STATUS_RENAMED=$(write '208' ' ')
+    GIT_STATUS_DELETED=$(write '161' '󰮉 ')
+    GIT_STATUS_STASHED=$(write '003' ' ')
+    GIT_STATUS_UNMERGED=$(write '016' '󰧁 ')
+    GIT_STATUS_AHEAD=$(write '012' ' ')
+    GIT_STATUS_BEHIND=$(write '011' ' ')
+    GIT_STATUS_DIVERGED=$(write '012' '󰧈 ')
+    GIT_STATUS_CLEAN=$(write '002' ' ')
+
+    INDEX=$(command git status --porcelain -b 2>/dev/null)
+
+    # Check for untracked files
+    if $(echo "$INDEX" | command grep -E '^\?\? ' &> /dev/null); then
+        git_status="$GIT_STATUS_UNTRACKED$git_status"
+    fi
+
+    # Check for staged files
+    if $(echo "$INDEX" | command grep '^A[ MDAU] ' &> /dev/null); then
+        git_status="$GIT_STATUS_ADDED$git_status"
+    elif $(echo "$INDEX" | command grep '^M[ MD] ' &> /dev/null); then
+        git_status="$GIT_STATUS_ADDED$git_status"
+    elif $(echo "$INDEX" | command grep '^UA' &> /dev/null); then
+        git_status="$GIT_STATUS_ADDED$git_status"
+    fi
+
+    # Check for modified files
+    if $(echo "$INDEX" | command grep '^[ MARC ]M ' &> /dev/null); then
+        git_status="$GIT_STATUS_MODIFIED$git_status"
+    fi
+
+    # Check for renamed files
+    if $(echo "$INDEX" | command grep '^R[ MD] ' &> /dev/null); then
+        git_status="$GIT_STATUS_RENAMED$git_status"
+    fi
+
+    # Check for deleted files
+    if $(echo "$INDEX" | command grep '^[MARCDU ]D ' &> /dev/null); then
+        git_status="$GIT_STATUS_DELETED$git_status"
+    elif $(echo "$INDEX" | command grep '^D[ UM] ' &> /dev/null); then
+        git_status="$GIT_STATUS_DELETED$git_status"
+    fi
+
+    # Check for stashes
+    if $(command git rev-parse --verify refs/stash >/dev/null 2>&1); then
+        git_status="$GIT_STATUS_STASHED$git_status"
+    fi
+
+    # Check for unmerged files
+    if $(echo "$INDEX" | command grep '^U[UDA] ' &> /dev/null); then
+        git_status="$GIT_STATUS_UNMERGED$git_status"
+    elif $(echo "$INDEX" | command grep '^AA ' &> /dev/null); then
+        git_status="$GIT_STATUS_UNMERGED$git_status"
+    elif $(echo "$INDEX" | command grep '^DD ' &> /dev/null); then
+        git_status="$GIT_STATUS_UNMERGED$git_status"
+    elif $(echo "$INDEX" | command grep '^[DA]U ' &> /dev/null); then
+        git_status="$GIT_STATUS_UNMERGED$git_status"
+    fi
+
+    # Check whether branch is ahead
+    local is_ahead=false
+    if $(echo "$INDEX" | command grep '^## [^ ]\+ .*ahead' &> /dev/null); then
+        is_ahead=true
+    fi
+
+    # Check whether branch is behind
+    local is_behind=false
+    if $(echo "$INDEX" | command grep '^## [^ ]\+ .*behind' &> /dev/null); then
+        is_behind=true
+    fi
+
+    # Check wheather branch has diverged
+    if [[ "$is_ahead" == true && "$is_behind" == true ]]; then
+        git_status="$GIT_STATUS_DIVERGED$git_status"
+    else
+        [[ "$is_ahead" == true ]] && git_status="$GIT_STATUS_AHEAD$git_status"
+        [[ "$is_behind" == true ]] && git_status="$GIT_STATUS_BEHIND$git_status"
+    fi
+
+    [[ -n "$git_status" ]] || git_status="$GIT_STATUS_CLEAN"
+
+    bold "$git_status"
+    write '241' "$git_branch"
 }
 
-export PROMPT='%(?.%F{205}.%F{red})⇨%f '
-export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `git_arrows``suspended_jobs`'
+async_init
+async_start_worker vcs_info
+async_register_callback vcs_info git_status_done
 
+add-zsh-hook precmd () {
+    print -P "\n\e[1m%F{075}%~\e[0m $(node_prompt)"
+    async_job vcs_info git_status "$PWD"
+}
+
+
+export PROMPT='%(?.%F{006}.%F{009})$PROMPT_SYMBOL%f '
+export RPROMPT="$(suspended_jobs)"
