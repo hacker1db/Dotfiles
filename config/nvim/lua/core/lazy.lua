60292c0713cf35b3e802a265c4e02cb578733cef
hacker1db <6212228+hacker1db@users.noreply.github.com>
2024-03-04 01:23:31 -0800

    checking conversion of config

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -12,6 +12,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "plugins" } }, {
+    opts = {
+        colorscheme = "dracula",
+    },
     install = {
         colorscheme = { "dracula" },
     },
--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -1,53 +1,53 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-	  local nvimtree = require("nvim-tree")
-  
-	  -- recommended settings from nvim-tree documentation
-	  vim.g.loaded_netrw = 1
-	  vim.g.loaded_netrwPlugin = 1
-	 	  -- configure nvim-tree
-	  nvimtree.setup({
-		view = {
-		  width = 35,
-		  relativenumber = true,
-		},
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local nvimtree = require("nvim-tree")
 
-		disable_netrw = false,
-		hijack_netrw = true,
-		diagnostics = {
-			enable = false,
-		},
-		update_focused_file = {
-			enable = true,
-			update_cwd = true,
-		},
-		git = {
-			enable = true,
-			ignore = false,
-		},
-		view = {
-			width = 40,
-			side = "left",
-		},
+        -- recommended settings from nvim-tree documentation
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        -- configure nvim-tree
+        nvimtree.setup({
+            view = {
+                width = 35,
+                relativenumber = true,
+            },
 
-		-- disable window_picker for
-		-- explorer to work well with
-		-- window splits
-		actions = {
-		  open_file = {
-			window_picker = {
-			  enable = false,
-			},
-		  },
-		},
-		filters = {
-		  custom = { ".DS_Store" },
-		},
-		git = {
-		  ignore = false,
-		},
-	  })
-	end,
-  }
+            disable_netrw = false,
+            hijack_netrw = true,
+            diagnostics = {
+                enable = false,
+            },
+            update_focused_file = {
+                enable = true,
+                update_cwd = true,
+            },
+            git = {
+                enable = true,
+                ignore = false,
+            },
+            view = {
+                width = 40,
+                side = "left",
+            },
+
+            -- disable window_picker for
+            -- explorer to work well with
+            -- window splits
+            actions = {
+                open_file = {
+                    window_picker = {
+                        enable = false,
+                    },
+                },
+            },
+            filters = {
+                custom = { ".DS_Store" },
+            },
+            git = {
+                ignore = false,
+            },
+        })
+    end,
+}
