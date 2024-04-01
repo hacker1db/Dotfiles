732b7bfc82de820af4b2e3c224a163dd339b2ce2
hacker1db <6212228+hacker1db@users.noreply.github.com>
2024-03-31 21:48:25 -0700

    added bun

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -79,6 +79,8 @@ brew tap anchore/syft
 brew install syft
 brew tap azure/functions
 brew install azure-functions-core-tools@4
+brew tap oven-sh/bun
+brew install bun
 # if upgrading on a machine that has 2.x or 3.x installed:
 brew link --overwrite azure-functions-core-tools@4
 
