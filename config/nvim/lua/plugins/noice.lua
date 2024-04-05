8ad5b2c973145ef53382ae822ae87b6b46727b5c
hacker1db <6212228+hacker1db@users.noreply.github.com>
2024-04-04 23:47:30 -0700

    got some noice noise going on

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -15,7 +15,6 @@ return {
             -- you can enable a preset for easier configuration
             presets = {
                 bottom_search = false, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true, -- add a border to hover docs and signature help
@@ -24,6 +23,39 @@ return {
                 {
                     filter = { find = "No information available" },
                     opts = { stop = true },
+                },
+            },
+            views = {
+                cmdline_popup = {
+                    border = {
+                        style = "rounded",
+                    },
+                    position = {
+                        row = 12,
+                        col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = "auto",
+                    },
+                },
+                popupmenu = {
+                    relative = "editor",
+                    position = {
+                        row = 14,
+                        col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = 10,
+                    },
+                    border = {
+                        style = "rounded",
+                        padding = { 0, 1 },
+                    },
+                    win_options = {
+                        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                    },
                 },
             },
         },
