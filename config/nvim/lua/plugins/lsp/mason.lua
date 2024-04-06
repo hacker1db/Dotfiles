8a9373c312d92e3ee171070d377c0733f1088acf
hacker1db <6212228+hacker1db@users.noreply.github.com>
2024-04-05 22:40:24 -0700

    confirmed mason is good

--- config/nvim/lua/plugins/lsp/mason.lua
+++ config/nvim/lua/plugins/lsp/mason.lua
@@ -1,52 +1,57 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-
+    "williamboman/mason.nvim",
+    dependencies = {
         "williamboman/mason-lspconfig.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-        },
-        config = function()
-            local mason_lspconfig = require("mason-lspconfig")
-            mason_lspconfig.setup({
-                -- list of servers for mason to install
-                ensure_installed = {
-                    "tsserver",
-                    "html",
-                    "cssls",
-                    "tailwindcss",
-                    "lua_ls",
-                    "omnisharp",
-                    "dockerls",
-                    "gopls",
-                    "golangci_lint_ls",
-                    "marksman",
-                    "powershell_es",
-                    "jedi_language_server",
-                    "sqlls",
-                    "yamlls",
-                    "terraformls",
-                    "omnisharp",
-                    "azure_pipelines_ls",
-                    "bashls",
-                    "tsserver",
-                    "tailwindcss",
-                    "svelte",
-                    "spectral",
-                    "jqls",
-                    "bashls",
-                    "mdx_analyzer",
-                    "tflint",
-                    "marksman",
-                },
-                -- auto-install configured servers (with lspconfig)
-                automatic_installation = true, -- not the same as ensure_installed
-            })
-        end,
     },
+    config = function()
+        -- import mason
+        local mason = require("mason")
+
+        -- import mason-lspconfig
+        local mason_lspconfig = require("mason-lspconfig")
+
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+
+        mason_lspconfig.setup({
+            -- list of servers for mason to install
+            ensure_installed = {
+                "tsserver",
+                "html",
+                "cssls",
+                "tailwindcss",
+                "lua_ls",
+                "omnisharp",
+                "dockerls",
+                "gopls",
+                "golangci_lint_ls",
+                "marksman",
+                "powershell_es",
+                "jedi_language_server",
+                "sqlls",
+                "yamlls",
+                "terraformls",
+                "omnisharp",
+                "azure_pipelines_ls",
+                "bashls",
+                "tsserver",
+                "tailwindcss",
+                "svelte",
+                "spectral",
+                "jqls",
+                "bashls",
+                "mdx_analyzer",
+                "tflint",
+                "marksman",
+            },
+        })
+    end,
 }
