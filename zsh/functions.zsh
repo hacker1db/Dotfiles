32211acf4c23d8bc3be6dee71ca8ff340ff90c61
CyberDSO <6212228+hacker1db@users.noreply.github.com>
2025-01-13 12:21:32 -0800

    check on todo comment

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -177,7 +177,7 @@ function azdlogin(){
 function gha(){
        gh project item-add 16 --owner Alaska-ITS --url "$@"
 }
-
+## TODO: Review this function with brew bundle dump --file=~/.dotfiles/install/brewfile --cleanup --zap --describe --force
 function brew-cleanup(){
  brew bundle dump --mas --tap --cask --describe --all -v --cleanup --file="~/.dotfiles/install/brewfile" -f  && brew cleanup && brew doctor
 }
