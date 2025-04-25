5f369832a6a6c928fc22341649c76145f11fdc09
CyberDSO <6212228+hacker1db@users.noreply.github.com>
2025-04-24 22:21:02 -0700

    added render markdown plugin

---
+++ config/nvim/lua/plugins/extra/rendermd.lua
@@ -0,0 +1,7 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
