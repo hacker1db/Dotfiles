6532a9f44ee196991fc5762b7327072b3345e915
hacker1db <hacker1db@hacker1db.dev>
2026-07-16 23:37:22 -0700

    address broken commit hooks

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -2,22 +2,26 @@
 
 EXIT_CODE=0
 
-repo=$( git rev-parse --show-toplevel )
-hook_type=$( basename $0 )
-hooks=~/.dotfiles/git/hooks
+hook_type=$(basename "$0")
+hooks="$HOME/.dotfiles/git/hooks"
 
 echo "Executing $hook_type hook(s)"
 
-for hook in $hooks/*.$hook_type; do
+for hook in "$hooks"/*."$hook_type"; do
+	[ -e "$hook" ] || continue
+
 	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
-	${hook}
-	EXIT_CODE=$((${EXIT_CODE} + $?))
+	echo "Executing $hook"
+	"$hook" "$@"
+	hook_exit_code=$?
+	if [ "$hook_exit_code" -ne 0 ]; then
+		EXIT_CODE=$hook_exit_code
+	fi
 done
 
-if [[ ${EXIT_CODE} -ne 0 ]]; then
+if [ "$EXIT_CODE" -ne 0 ]; then
 	echo ""
-	echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
+	echo "$hook_type hook failed."
 fi
 
-exit $((${EXIT_CODE}))
+exit "$EXIT_CODE"
